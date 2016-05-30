e5d24d6db6ee0922af25aade8af28c88354558a8
Anshul Singh <anshul@neevtech.com>
2016-05-30 15:55:12 +0530

    spec changes

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+//Server using express module of node
 var express = require('express'),
-    app = express();
+    server = express();
 
-app.use(express.static(__dirname + ''));
+server.use(express.static(__dirname + ''));
+server.listen(1991);
 
-app.listen(1991);
+//Example of how to create server using http module of node
+// require('http').createServer(function(request, response) {
+//   console.log('i got request.. sending response now..');
+// response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write('i m in..');
+//   response.end();
+// }).listen(3000);
